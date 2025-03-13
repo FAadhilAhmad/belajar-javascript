1666fee76d61d646a014897731c64e655c23c384
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-03-13 22:28:38 +0700

    belajar java script
    belajar tentang variabel pop up alert, dan promt

---
+++ belajar javascript/main.js
@@ -0,0 +1,28 @@
+var x = 10;
+var umur = 20;
+var nama = 'fadhil'
+var alamat = 'kampung bojong desa cigentur'
+console.log('helo wolrd!!');
+console.log('hello wolrd variabel dari x adalah ' + x);
+console.log('hallo umur saya adalah ' +  umur);
+console.log('halo nama saya ' + nama)
+console.log( nama + ' adalah nama saya dan umur saya adalah ' + umur)
+console.log('dan saya tinggal di ' + alamat)
+
+// pembelajaran tentang pop up 
+alert('hello wolrd');
+alert('hallo gays selamat datang');
+alert('di web pertama saya ');
+alert('halo gays nama saya ' + nama);
+alert('dan umur saya ' + umur);
+alert('dan saya tinggal di ' + alamat);
+alert('terimakasih 😽🤗'); 
+
+// belajar popup prmont dan mengkombinasikan dengan variabel
+var kamu = prompt('masukan nama');
+alert('halo ' + kamu);
+var dimana = prompt('dimana kamu tinggal');
+alert('oke ' + kamu + ' kamu tinggal di ' + dimana);
+var kabar = prompt('bagaimana kabar mu ' + kamu);
+alert('ouh jadi kamu merasa ' + kabar);
+alert('oke nama kamu ' + kamu +' dan kamu tinggal di ' + dimana + ' dan sekarang kabar kamu ' + kabar)
